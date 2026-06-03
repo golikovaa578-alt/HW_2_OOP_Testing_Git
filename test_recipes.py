f9261e58cc455d0e362d234f58bfe287b41772a1
Фамилия Имя <you@edu.hse.ru>
2026-06-03 23:23:00 +0300

    Добавление файла test_recipes.py

--- test_recipes.py
+++ test_recipes.py
@@ -6,16 +6,13 @@ def test_ingredient_init():
     assert a.name == "Мука"
     assert a.quantity == 500.0
     assert a.unit == "г"
-
 def test_ingredient_str():
     a = Ingredient("Мука", 500, "г")
     assert str(a) == "Мука: 500.0 г"
-
 def test_ingredient_eq_same():
     a = Ingredient("Мука", 500, "г")
     b = Ingredient("Мука", 200, "г")
     assert a == b
-
 def test_ingredient_eq_different_name():
     a = Ingredient("Мука", 500, "г")
     b = Ingredient("Сахар", 500, "г")
@@ -26,7 +23,7 @@ def test_ingredient_eq_different_unit():
     b = Ingredient("Мука", 500, "кг")
     assert a != b
 
-# 2.2 Тесты Recipe
+# 2.2 Тесты
 def test_recipe_init():
     a = Recipe("Пицца")
     assert a.title == "Пицца"
